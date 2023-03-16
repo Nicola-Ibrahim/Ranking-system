2688968e5c0e2b9b3d096a6a16c2f068b7cd93c3
Nicola <nqola.ibrahim@lit-co.net>
2023-03-16 08:42:31 +0200

    Edit raw file path

--- ranking/src/settings.py
+++ ranking/src/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-RAW_SPACES_DATA_PATH = BASE_DIR / "data/raw/spaces_dummy_data_v2.json"
+RAW_SPACES_DATA_PATH = BASE_DIR / "data/raw/spaces_dummy_data_v3.json"
 PRE_PROC_SPACES_TIME_PATH = BASE_DIR / "data/interim/spaces_encoded_data.json"
 PROC_SPACES_DATA_PATH = BASE_DIR / "data/processed/spaces_processed_data.json"
 DECISION_MATRIX_PATH = BASE_DIR / "data/processed/decision_matrix.pkl"
