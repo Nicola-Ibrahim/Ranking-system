b42f1133aa552a9224d19f4131aa51f0ff301064
Nicola <nqola.ibrahim@lit-co.net>
2023-03-24 11:50:55 +0200

    Reset the runnable method

--- ranking/src/app.py
+++ ranking/src/app.py
@@ -9,15 +9,16 @@ def run():
     goal_time = 8  # type: ignore
 
     start = time.time()
-    # weights = [69.2, 23.1, 7.7]  # CR = 0%
-    # t = rank.Topsis(decision_matrix, weights, criteria=[False, False, False])
 
     # Read spaces data file
     spaces_data = utils.read_json_file(settings.RAW_SPACES_DATA_PATH)
 
     pipe_line = Pipeline(steps=transformers.pipe_line_steps)
-    pipe_line.set_params(make_decision_matrix__kw_args={"goal_time": goal_time})
+    print(pipe_line.get_params())
+    # pipe_line.set_params(add_distance_variable__kw_args={"goal_time": goal_time})
 
     data = pipe_line.fit_transform(X=spaces_data)
+    print(data)
+
     end = time.time()
     print("Execution time:", end - start)
